1e3613ae85dbb73820146aa6d2f248e124f03792
Merve Aydin <110039353+merveaydincode@users.noreply.github.com>
2024-10-07 18:30:04 +0300

    tailwind kütüphanesi kuruldu.

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import tw from 'twrnc'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View style={tw`flex-1 justify-center items-center bg-black`}>
+    <Text style={tw`text-md text-white`}>Tailwinddddd</Text>
+  </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
