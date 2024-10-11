8a21cca4d9094be27a024668dee5a83696d2ea83
Merve Aydin <110039353+merveaydincode@users.noreply.github.com>
2024-10-11 15:53:47 +0300

    navigation paketi eklendi. root navigation dosyasına register ekranı entegre edildi.

---
+++ client/src/navigation/RootNavigation.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import RegisterScreen from '../screens/RegisterScreen';
+
+const Stack = createStackNavigator();
+
+const RootNavigation: React.FC = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Register">
+        <Stack.Screen name="Register" component={RegisterScreen} options={{ title: 'Kayıt Ol' }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default RootNavigation;
