4e0964e540f6d229a19d3a832d17bfa1b1b080b9
Merve Aydin <110039353+merveaydincode@users.noreply.github.com>
2024-10-15 12:23:35 +0300

    signup card background rengi değiştirildi.

--- client/src/screens/RegisterScreen.tsx
+++ client/src/screens/RegisterScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, Alert } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Alert } from 'react-native';
 import tw from 'twrnc';
 
 const RegisterScreen: React.FC = () => {
@@ -12,33 +12,39 @@ const RegisterScreen: React.FC = () => {
   };
 
   return (
-    <View style={tw`flex-1 justify-center items-center p-4 bg-white`}>
-      <Text style={tw`text-2xl font-bold mb-4`}>Kayıt Ol</Text>
-      
-      <TextInput
-        style={tw`border border-gray-300 rounded p-2 w-full mb-4`}
-        placeholder="Ad"
-        value={name}
-        onChangeText={setName}
-      />
-      
-      <TextInput
-        style={tw`border border-gray-300 rounded p-2 w-full mb-4`}
-        placeholder="E-posta"
-        value={email}
-        onChangeText={setEmail}
-        keyboardType="email-address"
-      />
-      
-      <TextInput
-        style={tw`border border-gray-300 rounded p-2 w-full mb-4`}
-        placeholder="Şifre"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-      />
-      
-      <Button title="Kayıt Ol" onPress={handleRegister} />
+    <View style={tw`flex-1 justify-center p-4 bg-black`}>
+           <Text style={tw`text-2xl font-bold mb-2 p-3 text-white`}>Sign Up</Text>
+      <View style={tw`bg-indigo-200 p-10 rounded-3xl w-full max-w-md`}>
+        <TextInput
+          style={tw`border border-white rounded-xl p-3 w-full mb-4`}
+          placeholder="Ad"
+          value={name}
+          onChangeText={setName}
+        />
+        
+        <TextInput
+          style={tw`border border-white rounded-xl p-3 w-full mb-4`}
+          placeholder="E-posta"
+          value={email}
+          onChangeText={setEmail}
+          keyboardType="email-address"
+        />
+        
+        <TextInput
+          style={tw`border border-white rounded-xl p-3 w-full mb-4`}
+          placeholder="Şifre"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+        
+        <TouchableOpacity
+          style={tw`bg-indigo-400 rounded-xl p-2 mt-4 w-40 mx-auto h-12 justify-center`}
+          onPress={handleRegister}
+        >
+          <Text style={tw`text-white text-center  `}>Kayıt Ol</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
