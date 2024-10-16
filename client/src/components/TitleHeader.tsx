79c8dbaa093bbc2301b031a68a63bcb208c9d41a
Merve Aydin <110039353+merveaydincode@users.noreply.github.com>
2024-10-16 16:23:23 +0300

    title componentine ayarlar butonu eklendi.

--- client/src/components/TitleHeader.tsx
+++ client/src/components/TitleHeader.tsx
@@ -4,19 +4,22 @@ import { useNavigation } from '@react-navigation/native';
 import tw from 'twrnc';
 import { Ionicons } from '@expo/vector-icons';
 
-const BackHeader: React.FC<{ title: string }> = ({ title }) => {
+const TitleHeader: React.FC<{ title: string }> = ({ title }) => {
     const navigation = useNavigation();
 
     return (
-        <View style={tw`flex-row items-center justify-start p-3 mt-10`}>
-          <TouchableOpacity onPress={() => navigation.goBack()}>
-            <Ionicons name="arrow-back" size={34} color="white" />
-          </TouchableOpacity>
-          <Text style={tw`text-white text-2xl font-bold ml-3`}>
-            {title}
-          </Text>
+        <View style={tw`flex-row items-center justify-between p-3 mt-10`}>
+            <TouchableOpacity onPress={() => navigation.goBack()}>
+                <Ionicons name="arrow-back" size={34} color="white" />
+            </TouchableOpacity>
+            <Text style={tw`text-white text-2xl font-bold`}>
+                {title}
+            </Text>
+            <TouchableOpacity onPress={() => navigation.navigate('Settings')}>
+                <Ionicons name="person-circle-outline" size={34} color="white" />
+            </TouchableOpacity>
         </View>
-      );
-    };
+    );
+};
 
-export default BackHeader;
+export default TitleHeader;
