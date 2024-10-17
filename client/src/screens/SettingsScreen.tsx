eb236cfa72a9f991e02460282cf1216a69394e47
Merve Aydin <110039353+merveaydincode@users.noreply.github.com>
2024-10-17 11:56:10 +0300

    title ve back arrowlu header ooluşturuldu.

--- client/src/screens/SettingsScreen.tsx
+++ client/src/screens/SettingsScreen.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import tw from 'twrnc';
+import TitleEmpty from '../components/TitleEmpty';
 
 const SettingsScreen: React.FC = () => {
 
 
     return (
-        <View style={tw`flex-1 bg-black justify-center items-center`}>
-            <Text style={styles.header}>Ayarlar</Text>
+        <View style={tw`flex-1 bg-black`}>
+            <TitleEmpty title="Ayarlar" />
             <View style={tw`bg-indigo-200 p-4 mt-4 rounded`}>
                 <Text style={styles.label}>Ad:</Text>
                 <Text style={styles.label}>E-posta:</Text>              
